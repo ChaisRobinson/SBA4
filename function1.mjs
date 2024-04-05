fde57ec4e1f62525305c8f205d92797d85d212cd
Chazz Robinson <chais5robinson@gmail.com>
2024-04-05 15:22:24 -0400

    got the charizard cards to print

--- function1.mjs
+++ function1.mjs
@@ -1,12 +1,17 @@
 export async function getPhotos() {
-    const container = document.querySelector(".pokeContainer");
-  
-    const response = await fetch("https://api.pokemontcg.io/v2/cards?q=name:charizard");
-    const photos = await response.json();
-  
-    photos.forEach(photo => {
-      const image = document.createElement("img");
-      image.src = photo.images.small;
-      container.appendChild(image);
-    });
-  }
+  const container = document.querySelector(".pokeContainer");
+  const response = await fetch(
+    "https://api.pokemontcg.io/v2/cards?q=name:charizard",
+    {
+      headers: {
+        "X-Api-Key": "04ca77ed-0f1b-4688-8f33-7ba0f71897c1",
+      },
+    }
+  );
+  const data = await response.json();
+  data.data.forEach((card) => {
+    const image = document.createElement("img");
+    image.src = card.images.small;
+    container.appendChild(image);
+  });
+}
